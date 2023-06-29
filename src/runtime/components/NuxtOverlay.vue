3d7709c5ce9cf5abd0d8847dc9132554799ca353
Henrique Van Klaveren <klaveren@gmail.com>
2023-06-29 14:23:39 -0300

    refactor(component): optimize code

--- src/runtime/components/NuxtOverlay.vue
+++ src/runtime/components/NuxtOverlay.vue
@@ -1,27 +1,29 @@
 <template>
   <Teleport to="body">
-    <div 
-      :id="`overlay-${queueName}`" 
-      :class="`overlay-container overlay-position-${props.position || $overlay.getConfig().position}`"
+    <div
+      :id="`overlay-${queueName}`"
+      :class="[
+        `overlay-container`,
+        `overlay-position-${position || $overlay.getConfig().position}`,
+      ]"
       :style="`width: ${width} !important`"
     >
       <div
-        v-for="itemQueue in $overlay.getQueue().filter(item => item.queueName === queueName)"
+        v-for="itemQueue in $overlay
+          .getQueue()
+          .filter((item: QueueItems) => item.queueName === queueName)"
         :id="itemQueue.id"
         :key="itemQueue.id"
         class="overlay-content"
         @click="onClickClose(itemQueue.id)"
       >
-        <slot
-          name="default"
-          :item-queue="itemQueue"
-        >
+        <slot name="default" :item-queue="itemQueue">
           <div>
             <h3>Provide your own customized content</h3>
-            itemQueue:
+            itemQueue Data:
             <pre style="font-size: 10px">
-            {{ itemQueue || '' }}
-          </pre>
+                {{ itemQueue || "" }}
+            </pre>
           </div>
         </slot>
       </div>
@@ -31,33 +33,34 @@
 
 <script setup lang="ts">
 import { useNuxtApp } from "#imports";
-  const { $overlay } = useNuxtApp()
+import { QueueItems } from "../types";
+const { $overlay } = useNuxtApp();
 
-const props = defineProps( {
-    position: {
-      type: String,
-      required: false,
-      default: 'top-center'
-    },
-    queueName: {
-      type: String,
-      required: false,
-      default: 'default'
-    },
-    width: {
-      type: [Number, String],
-      required: false,
-      default: '33vw'
+const props = defineProps({
+  position: {
+    type: String,
+    required: false,
+    default: "top-center",
   },
-    closeOnClick: {
-      type: Boolean,
-      required: false
-    }
-  } )
-  async function onClickClose(id) {
-    if ( props.closeOnClick || $overlay.getConfig().closeOnClick)
-      await $overlay.remove( id, 1 )
-  }
+  queueName: {
+    type: String,
+    required: false,
+    default: "default",
+  },
+  width: {
+    type: [Number, String],
+    required: false,
+    default: "33vw",
+  },
+  closeOnClick: {
+    type: Boolean,
+    required: false,
+  },
+});
+async function onClickClose(id: string) {
+  if (props.closeOnClick || $overlay.getConfig().closeOnClick)
+    await $overlay.remove(id, 1);
+}
 </script>
 
 <style scoped>
@@ -68,5 +71,5 @@ const props = defineProps( {
 
 .overlay-slot:hover {
   cursor: pointer;
-} 
+}
 </style>
